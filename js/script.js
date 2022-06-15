5667f1631bd21531a0d32e41d0e82d6d6d895ff2
Franpan94 <francescopanico94@libero.it>
2022-06-15 17:09:52 +0200

    start mailstone 2

--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
-let generator= document.querySelector('#gene-rator');
-let output=  document.querySelector('#out-put');
-let age= document.querySelector('#age-passenger');
-let km= document.querySelector('#number-km');
+
+// Definiamo un bottone 
+let generator= document.getElementById('generator-btn');
+
+// A questo bottone gli aggiungiamo una lista di eventi
+generator.addEventListener('click', function(){
+   
+//  definiamo le variabili all'interno dell'evento
+  
+let km= document.getElementById('number-km').value;
+  console.log('km');
+  console.log(km);
+ 
+  let price= km * 0.26;
+  let output= document.getElementById('output-ticket');
+
+  let age= document.getElementById('age-passenger').value;
+  console.log('età');
+  console.log(age);
+
+  
+  // Impostiamo le varie condizioni
+  if(age < 18){
+    
+    priceunder= price - (price * 0.15);
+    priceunder= priceunder.toFixed(2);
+    output.innerHTML = priceunder;
+    console.log('prezzo scontato del 15%'); 
+    console.log(priceunder);
+  
+  } else if (age >= 65){
+
+    priceover= price - (price * 0.35);
+    priceover= priceover.toFixed(2);
+    output.innerHTML = priceover; 
+    console.log('prezzo scontato del 35%');
+    console.log(priceover);
+  
+  } else{
+    
+    price= price.toFixed(2);
+    output.innerHTML = price;
+    console.log('prezzo normale');
+    console.log(price);
+
+  }
+
+});
 
 
-if(age < 18){
-  let generator= document.querySelector('#gene-rator');
-  let output=  document.querySelector('#out-put');
-  let age= document.querySelector('#age-passenger');
-  let km= document.querySelector('#number-km');
 
-  generator.addEventListener('click', function(){
-    console.log (document.getElementById('age-passenger').value);
-    console.log (document.getElementById('number-km').value);
-    output.innerHTML += ((document.getElementById('number-km').value * 0.26) - ((document.getElementById('number-km').value * 0.26) * 0.15)) ;
-  })
-}
 
-  generator.addEventListener('click', function(){
-    console.log (document.getElementById('age-passenger').value);
-    console.log (document.getElementById('number-km').value);
-    output.innerHTML += document.getElementById('number-km').value * 0.26;
-  })
+
+
+
+
+
+
 
