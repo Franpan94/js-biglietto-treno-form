bd3714b13764db51d34e62c5b090e8346caf1ed2
Franpan94 <francescopanico94@libero.it>
2022-06-16 13:14:54 +0200

    exercise ended

--- js/script.js
+++ js/script.js
@@ -1,48 +1,60 @@
 
-// Definiamo un bottone 
+// Definiamo i bottoni 
 let generator= document.getElementById('generator-btn');
+let regenerator= document.getElementById('generator-btnr');
 
-// A questo bottone gli aggiungiamo una lista di eventi
+// Aggiungiamo una lista di eventi al bottone per generare il numero della carroza treno
+regenerator.addEventListener('click', function(){
+
+// Definiamo le variabili all'interno dell'evento
+let casual= Math.floor((Math.random() * 10) + 1);
+let number= document.getElementById('number-random');
+number.innerHTML = 'Il numero della carrozza treno è '+ casual;
+})
+
+
+// Aggiungiamo all'altro bottone una serie di eventi per generare il prezzo del biglietto
 generator.addEventListener('click', function(){
    
-//  definiamo le variabili all'interno dell'evento
-  
+// Definiamo le variabili all'interno dell'evento
 let km= document.getElementById('number-km').value;
-  console.log('km');
-  console.log(km);
+console.log('km');
+console.log(km);
  
-  let price= km * 0.26;
-  let output= document.getElementById('output-ticket');
+let price= km * 0.26;
+let output= document.getElementById('output-ticket');
 
-  let age= document.getElementById('age-passenger').value;
-  console.log('età');
-  console.log(age);
+let age= document.getElementById('age-passenger').value;
+console.log('età');
+console.log(age);
+
+
   
-  // Impostiamo le varie condizioni
-  if(age < 18){
+// Impostiamo le varie condizioni
+if(age < 18){
     
-    priceunder= price - (price * 0.15);
-    priceunder= priceunder.toFixed(2);
-    output.innerHTML = priceunder;
-    console.log('prezzo scontato del 15%'); 
-    console.log(priceunder);
+  priceunder= price - (price * 0.15);
+  priceunder= priceunder.toFixed(2);
+  output.innerHTML = 'Il prezzo del biglietto scontato del 15% riservato ai minorenni è di € '+ priceunder;
+  console.log('prezzo scontato del 15%'); 
+  console.log(priceunder);
   
-  } else if (age >= 65){
+} else if (age >= 65){
 
-    priceover= price - (price * 0.35);
-    priceover= priceover.toFixed(2);
-    output.innerHTML = priceover; 
-    console.log('prezzo scontato del 35%');
-    console.log(priceover);
+  priceover= price - (price * 0.35);
+  priceover= priceover.toFixed(2);
+  output.innerHTML = 'Il prezzo del biglietto scontato del 35% riservato agli over 65 è di € '+ priceover; 
+  console.log('prezzo scontato del 35%');
+  console.log(priceover);
   
-  } else{
+} else{
     
-    price= price.toFixed(2);
-    output.innerHTML = price;
-    console.log('prezzo normale');
-    console.log(price);
+  price= price.toFixed(2);
+  output.innerHTML = 'il prezzo del biglietto è di €'+ price;
+  console.log('prezzo normale');
+  console.log(price);
 
-  }
+}
 
 });
 
